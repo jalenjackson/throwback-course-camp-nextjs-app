a0afb1f733b254f072795d843116c6f6c5b84cf7
Jalen Jackson <jalenjack201@hotmail.com>
2019-01-30 00:56:39 -0500

    footer fix

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9fff73ac27d843943d9a";
+/******/ 	var hotCurrentHash = "57aeae6946fe72350aac";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
